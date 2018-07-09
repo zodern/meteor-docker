e88156e420ca0d46a17b97be57e9e080df14071a
zodern <zodern@users.noreply.github.com>
2018-07-09 14:08:57 -0500

    feat: Symlink duplicate npm versions

--- image/setup/install_node.js
+++ image/setup/install_node.js
@@ -1,11 +1,10 @@
 var versions = require('./versions.json');
-
 var { spawnSync, execSync } = require('child_process');
+var fs = require('fs');
 
 var nvm = '. "$NVM_DIR/nvm.sh" && nvm';
 
 process.env.PATH += ':/home/app/.nvm';
-
 console.log('\nInstalling node versions');
 
 function log({ output }) {
@@ -23,13 +22,19 @@ for (var i = 0; i < versions.length; i++) {
   });
   log(output);
 
-  var useOutput = spawnSync(nvm, ['use', version.node], {
-    stdio: 'pipe',
-    shell: true
-  });
-  log(useOutput);
+  if (version.npm === (versions[i - 1] ? versions[i - 1].npm : '')) {
+    console.log('symlinking duplicate npm version');
+    const prevVersion = versions[i - 1];
 
-  if (version.npm !== 'default') {
+    // This is only correct when the version.node is 0.10.x since nvm stores
+    // newer versions in a different location
+    const targetPath = `/home/app/.nvm/v${version.node}/lib/node_modules/npm`;
+    execSync(`rm -rf ${targetPath}`);
+    fs.symlinkSync(
+      `/home/app/.nvm/versions/node/v${prevVersion.node}/lib/node_modules/npm`,
+      targetPath
+    );
+  } else if (version.npm !== 'default') {
     var npmOutput = execSync(`${nvm} use ${version.node} && npm install --global npm@${version.npm}`, {
       stdio: 'pipe'
     });
