97c9c4d8187ef7197f3727af1969aae682886b65
zodern <zodern@users.noreply.github.com>
2020-12-31 17:38:10 -0600

    chore: Add new Meteor versions to tests

--- tests/test.sh
+++ tests/test.sh
@@ -166,7 +166,14 @@ test_versions() {
   # test_version "--release=1.4"
   # test_version "--release=1.4.4.5"
   test_version "--release=1.5.4.1"
-  test_version "--release=1.6"
+  test_version "--release=1.6.1.4"
+  test_version "--release=1.7.0.5"
+  test_version "--release=1.8.1"
+  test_version "--release=1.9.3"
+  test_version "--relese=1.10.2"
+  test_version "--release=1.11.1"
+  test_version "--release=1.12"
+  test_version "--release=2.0-beta.3"
 
   # Latest version
   test_version
